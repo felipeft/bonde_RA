b4a01e22487464882aab3216d35130751d261573
felipeft_ <50221269+felipeft@users.noreply.github.com>
2026-06-22 19:08:16 -0300

    Update gps.js

--- js/gps.js
+++ js/gps.js
@@ -1,7 +1,5 @@
 /**
  * gps.js
- * Como a câmera já está rastreando o GPS via HTML, este arquivo agora foca
- * exclusivamente em "ancorar" o bonde no mundo real ou soltá-lo para o Modo Livre.
  */
 export class GPSController {
   constructor(modelController) {
@@ -14,15 +12,13 @@ export class GPSController {
     if (this.modelController && this.modelController.entity) {
       const entity = this.modelController.entity;
 
-      // 1. Remove a posição livre do A-Frame para não haver conflito
       entity.removeAttribute('position');
       
-      // 2. Aplica a escala e rotação padronizada do ambiente de rua (passada pelos colegas)
       entity.setAttribute('scale', '0.5 0.5 0.5');
       entity.setAttribute('rotation', '0 0 0');
 
-      // 3. Ancora o modelo na coordenada GPS real informada pelos seus colegas
-      entity.setAttribute('gps-new-entity-place', 'latitude: -5.120145; longitude: -38.355598');
+      // as coordenadas devem ser colocadas aqui
+      entity.setAttribute('gps-new-entity-place', 'latitude: -4.9724306; longitude: -39.0209987');
     }
   }
 
@@ -32,10 +28,8 @@ export class GPSController {
     if (this.modelController && this.modelController.entity) {
       const entity = this.modelController.entity;
 
-      // 1. Solta o bonde das coordenadas geográficas
       entity.removeAttribute('gps-new-entity-place');
       
-      // 2. Aciona a função que teleporta o bonde de volta para a frente da câmera (Modo Livre)
       this.modelController.reset();
     }
   }
